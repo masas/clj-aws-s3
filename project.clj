6ebb4d6d2fda797441cac9daf4298cbad07c925f
Masashi Shimizu <cp.masas@gmail.com>
2017-05-17 21:05:03 +0900

    update aws sdk for faraday

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject org.clojars.masas/clj-aws-s3 "0.3.10"
+(defproject org.clojars.masas/clj-aws-s3 "0.3.11"
   :description "Clojure Amazon S3 library"
   :url "https://github.com/weavejester/clj-aws-s3"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.2.1"]
-                 [com.amazonaws/aws-java-sdk "1.7.5" :exclusions [joda-time]]
-                 [clj-time "0.11.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.amazonaws/aws-java-sdk "1.10.49" :exclusions [joda-time]]
+                 [clj-time "0.13.0"]]
   :plugins [[codox "0.8.10"]])
